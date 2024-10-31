b7e4374bcbd8d927ec06bb216f047e5bf1effe6d
KavinduLakshanFernando <kavindu2003fernando@gmail.com>
2024-10-31 20:45:56 +0530

    SPA

---
+++ assets/Js/Order.js
@@ -0,0 +1,2 @@
+import OrderModels from "../../models/OrderModels.js"
+import {order_array} from "../../db/database.js";
---
+++ models/OrderModels.js
@@ -0,0 +1,4 @@
+export default class OrderModels{
+    constructor(id,date,) {
+    }
+}
